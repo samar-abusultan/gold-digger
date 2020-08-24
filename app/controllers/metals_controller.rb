c6613092a7e6cea7ad7b2d64fb387ea2d661e944
Samar <samar.abusultan@hungerstation.com>
2020-08-24 11:16:14 +0300

    feat(metals): add http client gem

--- app/controllers/metals_controller.rb
+++ app/controllers/metals_controller.rb
@@ -1,7 +1,9 @@
+require 'httparty'
 class MetalsController < ApplicationController
 
   def index
-    render :json => {success: true}
+    response = HTTParty.get("https://metals-api.com/api/latest?access_key=#{Rails.application.credentials.metal_key}&base=SAR&symbols=XAU,XAG")
+    render :json => {success: true, key: Rails.application.credentials.metal_key, response: response}
   end
 
 end
